b8314dc69e43d1976779df7af561cc5d44d8e328
akshaykalambe <akshaykalambe10@gmail.com>
2021-01-01 22:27:23 +0530

    guess the number

--- fibo/fibo.js
+++ fibo/fibo.js
@@ -1,29 +1,19 @@
 
+let fibo = (number) => {
 
-// this is easy a easy approach where n is number of place of which
-// we want to find fibonacci
-
-// eg : n=5 we know  as we know addition of previous and number before previous is owr output
-
-// make function
-
-function fibonaccis(n) {
-// Define three variable for number 1 , number 2 and for next number
-    let numberFirst = 0;
-    let numberSecond = 1;
+    let firstNumber = 0;
+    let secondNumber = 1;
     let next;
+    for(let i=0; i<= number ; i++) {
 
 
-    for (let i = 0; i <= n; i++) {
 
+        console.log(firstNumber)
+        next= firstNumber+secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = next;
 
-        console.log(numberFirst)
-        next= numberFirst+ numberSecond;
-        numberFirst = numberSecond;
-        numberSecond = next;
+    }
 
 }
-
-}
-
-fibonaccis(20)
+fibo(10)
